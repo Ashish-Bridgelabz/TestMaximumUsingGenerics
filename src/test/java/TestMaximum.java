737fe95c8911ea422b141969d03847a4c5653f8d
Devki1 <gdev3195@gmail.com>
2020-03-13 19:30:26 +0530

    ADDED:Test case to finded maximum string

--- src/test/java/TestMaximum.java
+++ src/test/java/TestMaximum.java
@@ -3,16 +3,23 @@ import org.junit.Test;
 
 public class TestMaximum {
     @Test
-    public void givenThreeInteger_WhenProper_MethodShouldReturnTrue() {
+    public void givenThreeInteger_WhenProper_MethodShouldReturnMaximum() {
         Maximum maximum = new Maximum();
         Integer maxInteger[] = {10, 20, 30};
         Assert.assertEquals(maximum.maximumInteger(maxInteger), maxInteger[2]);
     }
 
     @Test
-    public void givenThreeFloatNumber_WhenProper_MethodShouldReturnTrue() {
+    public void givenThreeFloatNumber_WhenProper_MethodShouldReturnMaximum() {
         Maximum maximum = new Maximum();
         Float maxFloat[] = {10.11f, 12.12f, 13.210f};
         Assert.assertEquals(maximum.maximumFloat(maxFloat), maxFloat[2]);
+    }
+
+    @Test
+    public void givenThreeString_WhenProper_MethodShouldReturnMaximum() {
+        Maximum maximum = new Maximum();
+        String maxString[] = {"ram", "abhi", "virat"};
+        Assert.assertEquals(maximum.maximumString(maxString), maxString[2]);
     }
 }
