fafb22f7adbe16b2a6be82f19482f3c090cb7ea7
Devki1 <gdev3195@gmail.com>
2020-03-13 19:24:02 +0530

    REFACTORED:Added to method find maximumString

--- src/main/java/Maximum.java
+++ src/main/java/Maximum.java
@@ -1,4 +1,5 @@
 public class Maximum {
+    //Find maximum integer value
     public static Integer maximumInteger(Integer[] maxInteger) {
         Integer maximum = 0;
         for (Integer index : maxInteger) {
@@ -7,10 +8,22 @@ public class Maximum {
         }
         return maximum;
     }
-    public static Float maximumFloat(Float[] maxFloat){
+
+    //Find maximum float value
+    public static Float maximumFloat(Float[] maxFloat) {
         Float maximum = 0.0f;
-        for (Float index : maxFloat)  {
-            if (index.compareTo(maximum)>0)
+        for (Float index : maxFloat) {
+            if (index.compareTo(maximum) > 0)
+                maximum = index;
+        }
+        return maximum;
+    }
+
+    //Find maximum string value
+    public static String maximumString(String[] maxString) {
+        String maximum = " ";
+        for (String index : maxString) {
+            if (index.compareTo(maximum) > 0)
                 maximum = index;
         }
         return maximum;
