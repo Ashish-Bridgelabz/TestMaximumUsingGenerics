3b1e3e79207bf045881d12f97e56a743f7fdda8d
Devki1 <gdev3195@gmail.com>
2020-03-14 14:46:36 +0530

    REFACTORED:Added one generic method instead of three individual method

--- src/main/java/Maximum.java
+++ src/main/java/Maximum.java
@@ -1,28 +1,8 @@
 public class Maximum {
-    //Find maximum integer value
-    public static Integer maximumInteger(Integer[] maxInteger) {
-        Integer maximum = 0;
-        for (Integer index : maxInteger) {
-            if (index.compareTo(maximum) > 0)
-                maximum = index;
-        }
-        return maximum;
-    }
 
-    //Find maximum float value
-    public static Float maximumFloat(Float[] maxFloat) {
-        Float maximum = 0.0f;
-        for (Float index : maxFloat) {
-            if (index.compareTo(maximum) > 0)
-                maximum = index;
-        }
-        return maximum;
-    }
-
-    //Find maximum string value
-    public static String maximumString(String[] maxString) {
-        String maximum = " ";
-        for (String index : maxString) {
+    public static <E extends Comparable> E maximum(E[] maxInteger) {
+        E maximum = maxInteger[0];
+        for (E index : maxInteger) {
             if (index.compareTo(maximum) > 0)
                 maximum = index;
         }
